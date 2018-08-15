86153df697c1663309af2aa8c8f2c7cfa3cd370a
tazozzang <dlwldus718@naver.com>
2018-08-15 14:08:00 +0900

    클래스 수정 가능

--- app/src/main/java/com/example/tazo/testforfirebase/Main2Activity.java
+++ app/src/main/java/com/example/tazo/testforfirebase/Main2Activity.java
@@ -10,6 +10,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
@@ -21,6 +24,7 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_main2);
         findViewById(R.id.btn_send).setOnClickListener(this);
         findViewById(R.id.btn_delete).setOnClickListener(this);
+        findViewById(R.id.btn_update).setOnClickListener(this);
         initFirebaseDatabase();
     }
     private void initFirebaseDatabase() {
@@ -58,18 +62,25 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
+        Qtqt qtqt;
         switch (v.getId()){
             case R.id.btn_send:
-                Qtqt qtqt = new Qtqt("이지연", 24.0 , "서울시 광진구");
-                databaseReference.child(qtqt.getName()).setValue(qtqt);
-                qtqt = new Qtqt("이미란", 24.0 , "서울시 광진구");
-                databaseReference.child(qtqt.getName()).setValue(qtqt);
-                qtqt = new Qtqt("전소은", 24.0 , "서울시 광진구");
-                databaseReference.child(qtqt.getName()).setValue(qtqt);
+                qtqt = new Qtqt("이지연", 24.0 , "서울시 광진구");
+                databaseReference.child("dlwldus718").setValue(qtqt);
+                qtqt = new Qtqt("이미란", 24.0 , "인천시 계양구");
+                databaseReference.child("ranz_").setValue(qtqt);
+                qtqt = new Qtqt("전소은", 24.0 , "고양시 일산");
+                databaseReference.child("jse525").setValue(qtqt);
                 break;
             case R.id.btn_delete:
-                databaseReference.child("이지연").removeValue();
+                databaseReference.child("dlwldus718").removeValue();
                 break;
+            case R.id.btn_update :
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put("dlwldus718/name","공호진");
+                databaseReference.updateChildren(map);
+                break;
+
         }
 
     }
